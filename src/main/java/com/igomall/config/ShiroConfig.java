c6ec5faac59f8426b4323cfed6b08410058f0e3d
1169794338@qq.com <Liziyi521521>
2019-10-31 11:04:13 +0800

    增加ehcache

--- src/main/java/com/igomall/config/ShiroConfig.java
+++ src/main/java/com/igomall/config/ShiroConfig.java
@@ -1,6 +1,9 @@
 package com.igomall.config;
 
+import com.igomall.entity.Admin;
+import com.igomall.security.AuthenticationFilter;
 import com.igomall.security.AuthorizingRealm;
+import com.igomall.security.LogoutFilter;
 import org.apache.shiro.cache.ehcache.EhCacheManager;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
@@ -9,6 +12,10 @@ import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.servlet.Filter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @Configuration
 public class ShiroConfig {
 
@@ -16,7 +23,15 @@ public class ShiroConfig {
     public ShiroFilterFactoryBean shiroFilter(@Qualifier("securityManager") DefaultWebSecurityManager securityManager){
         ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
         shiroFilterFactoryBean.setSecurityManager(securityManager);
+        shiroFilterFactoryBean.setUnauthorizedUrl("/common/error/unauthorized");
 
+        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+
+        Map<String, Filter> filters = new LinkedHashMap<>();
+        filters.put("adminAuthc",adminAuthc());
+        filters.put("logout",logout());
+        shiroFilterFactoryBean.setFilters(filters);
 
         return shiroFilterFactoryBean;
     }
@@ -42,5 +57,20 @@ public class ShiroConfig {
         EhCacheManager shiroCacheManager = new EhCacheManager();
         shiroCacheManager.setCacheManager(ehCacheManager.getObject());
         return shiroCacheManager;
+    }
+
+
+    @Bean
+    public AuthenticationFilter adminAuthc(){
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setUserClass(Admin.class);
+        authenticationFilter.setLoginUrl("/admin/login");
+        authenticationFilter.setSuccessUrl("/admin/index");
+        return authenticationFilter;
+    }
+
+    @Bean
+    public LogoutFilter logout(){
+       return new LogoutFilter();
     }
 }
