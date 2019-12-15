5131bfb01dd3df4f76a62ba121db7849fbd86b15
1169794338@qq.com <1169794338@qq.com>
2019-12-15 21:10:01 +0800

    init

--- src/main/java/com/igomall/controller/admin/course/ChapterController.java
+++ src/main/java/com/igomall/controller/admin/course/ChapterController.java
@@ -33,6 +33,7 @@ public class ChapterController extends BaseController {
     public Message save(Chapter chapter,Long partId, Long courseId){
         chapter.setPart(partService.find(partId));
         chapter.setCourse(courseService.find(courseId));
+        chapter.setTeacher(chapter.getCourse().getTeacher());
         if(!isValid(chapter)){
             return Message.error("参数错误");
         }
